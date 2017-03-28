7db0991a8842e0f0ed99047d5ccc0757bc7a566b
take-iwa <h.ooiwa7701@gmail.com>
2017-03-28 15:28:45 +0900

    TCP自局ポート指定２

--- Bridge-full/TcpManager.cs
+++ Bridge-full/TcpManager.cs
@@ -14,13 +14,16 @@ namespace Bridge_full
         {
 
         }
-        
+
         public void CloseCurrentSession()
         {
-            stream.Close();
-            tcpClient.Close();
-            stream = null;
-            tcpClient = null;
+            if (stream != null)
+            {
+                stream.Close();
+                tcpClient.Close();
+                stream = null;
+                tcpClient = null;
+            }
         }
 
         /*------------------------------------*/
@@ -35,16 +38,25 @@ namespace Bridge_full
                 // 接続
                 if (tcpClient == null)
                 {
-                    tcpClient = new TcpClient(server, port);
+                    IPEndPoint ipLocalEndPoint = new IPEndPoint(IPAddress.Any, 2049);
+                    TcpClient tcpClient = new TcpClient(ipLocalEndPoint);
+                    tcpClient.Connect(server, port);
+
+                    //tcpClient = new TcpClient(server, port);
+                    Console.WriteLine("サーバー({0}:{1})と接続しました({2}:{3})。",
+                    ((System.Net.IPEndPoint)tcpClient.Client.RemoteEndPoint).Address,
+                    ((System.Net.IPEndPoint)tcpClient.Client.RemoteEndPoint).Port,
+                    ((System.Net.IPEndPoint)tcpClient.Client.LocalEndPoint).Address,
+                    ((System.Net.IPEndPoint)tcpClient.Client.LocalEndPoint).Port);
                     stream = tcpClient.GetStream();
                 }
-                
+
                 // コマンド送信
                 stream.Write(command, 0, command.Length);
 
                 // レスポンス受信
                 Int32 resSize = stream.Read(data, 0, data.Length);
-                if(resSize ==0)
+                if (resSize == 0)
                 {
                     MessageBox.Show("Error: Recv Size = 0\r\n");
                 }
@@ -52,6 +64,11 @@ namespace Bridge_full
             catch (Exception e)
             {
                 MessageBox.Show("Message: " + e.Message + "\n" + "StackTrace: " + e.StackTrace + "\r\n");
+            }
+            finally
+            {
+                // Close everything.
+                CloseCurrentSession();
             }
 
             return data;
